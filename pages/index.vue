3c8f33d3ef32778a1f484a06cc92c029b3daca04
funnythingz <funnythingz69+github@gmail.com>
2019-10-08 12:27:39 +0900

    install nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.container
+  Logo
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
