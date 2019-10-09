55a2e2cc4a183012f69b4ef3925f8e2ebb3d1d92
funnythingz <funnythingz69+github@gmail.com>
2019-10-09 19:20:10 +0900

    added users/_id

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,10 +1,50 @@
 <template lang="pug">
 .container
-  h1
-    | profile
+  dl
+    dt
+      | 名前
+    dd
+      | {{user.last_name}} {{user.first_name}}
+  dl
+    dt
+      | 生年月日
+    dd
+      | {{user.birth_month}}/{{user.birth_day}}/{{user.birth_year}}
 </template>
 
 <script>
+import { db } from '~/plugins/firestore.js'
+import { mapState } from 'vuex'
+import { isEmpty } from 'lodash/lodash'
+
+export default {
+
+  async created() {
+    const doc = await db.collection('users').doc(this.$route.params.id).get()
+    if (this.isLogin() && !isEmpty(doc.data())) {
+      this.user = doc.data()
+      return
+    }
+    this.$router.push({path: '/'})
+    return
+  },
+
+  data() {
+    return {
+      user: {}
+    }
+  },
+
+  computed: {
+    ...mapState(['currentUser'])
+  },
+
+  methods: {
+    isLogin() {
+      return !isEmpty(this.currentUser)
+    }
+  }
+}
 </script>
 
 <style lang="sass" scoped>
