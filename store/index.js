a7f020227ed0edd35b64f638111c67a791864866
funnythingz <funnythingz69+github@gmail.com>
2019-10-09 16:18:49 +0900

    Store is module mode

--- store/index.js
+++ store/index.js
@@ -1,32 +1,29 @@
 import Vuex from 'vuex'
 import { vuexfireMutations, firestoreAction } from 'vuexfire'
 
-const Store = () => {
-  return new Vuex.Store({
-    state: {
-      currentUser: {},
-      organizations: []
-    },
-    mutations: {
-      ...vuexfireMutations,
-      setCurrentUser(state, user) {
+export const state = () => ({
+    currentUser: {},
+    organizations: []
+})
+
+export const mutations = {
+    ...vuexfireMutations,
+    setCurrentUser(state, user) {
         state.currentUser = user
-      },
-    },
-    actions: {
-      setOrganizationsRef: firestoreAction(({ bindFirestoreRef }, ref) => {
-        bindFirestoreRef('organizations', ref)
-      })
-    },
-    getters: {
-      getCurrentUser: (state) => {
-        return state.currentUser
-      },
-      getOrganizations: (state) => {
-        return state.organizations
-      },
-    },
-  })
+    }
 }
 
-export default Store
+export const actions = {
+    setOrganizationsRef: firestoreAction(({ bindFirestoreRef }, ref) => {
+        bindFirestoreRef('organizations', ref)
+    })
+}
+
+export const getters = {
+    getCurrentUser: (state) => {
+        return state.currentUser
+    },
+    getOrganizations: (state) => {
+        return state.organizations
+    }
+}
